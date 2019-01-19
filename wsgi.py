7d1302011c1cfc14246c1762fc1866f1bb9b8574
Elvis <celvischuks@gmail.com>
2019-01-19 13:07:39 +0100

    changed hello

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Heyy this is App-lee!"
 
 if __name__ == "__main__":
     application.run()
