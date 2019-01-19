2e012e0342e570a81d2c7b5d32462ee7f1bdff94
Elvis <celvischuks@gmail.com>
2019-01-19 15:52:07 +0100

    test google api

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,26 @@
 from flask import Flask
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route("/")
+@app.route("/")
 def hello():
-    return "Heyy this is App-lee!"
+    import pprint
+    from apiclient.discovery import build
+    import socket
+            #make api call to googles urlTestingTools
+    API_KEY = 'AIzaSyACKc0SQ80qbBwgVnJUaBq_XVrmRHr8Ymw'
+    service = build('searchconsole', 'v1', developerKey=API_KEY)
+    url = 'https://google.com'
+    params = {
+                'url':url,
+                'requestScreenshot':True,
+                }
+    req = service.urlTestingTools().mobileFriendlyTest().run(body=params, x__xgafv=None)
+    try:
+        response = req.execute()
+        if response['mobileFriendliness'] == 'MOBILE_FRIENDLY':
+            return 'mobile friendly'
+    except socket.timeout:
+        return 'there appears to be a problem at the moment, please try again later'
 
 if __name__ == "__main__":
-    application.run()
+    app.run()
